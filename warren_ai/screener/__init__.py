a292f6f59988771ed67e87aba0d294e63ff94210
kurokishi <kuroreidho@gmail.com>
2025-11-29 15:09:37 +0700

    Update __init__.py

--- warren_ai/screener/__init__.py
+++ warren_ai/screener/__init__.py
@@ -1,10 +1,11 @@
 """
-Stock screeners:
-- Sequential screener
-- Parallel screener
+Batch stock screening and parallel processing engines.
 """
 
 from .engine import ScreenerEngine
 from .parallel_engine import ParallelScreener
 
-__all__ = ["ScreenerEngine", "ParallelScreener"]
+__all__ = [
+    "ScreenerEngine",
+    "ParallelScreener", 
+]
