d710e9e608d64c00c7017563c724ff9a127ff433
kurokishi <kuroreidho@gmail.com>
2025-12-01 11:13:39 +0700

    Delete warren_ai directory

--- warren_ai/core/dividend.py
+++
@@ -1,4 +0,0 @@
-class DividendEngine:
-    def analyze(self, info: dict) -> dict:
-        dy = info.get("dividendYield")
-        return {"Yield": dy}
--- warren_ai/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Domain models for WarrenAI package.
-This __init__ should only export symbols and NOT execute runtime code.
-"""
-
-from .stock import StockAnalyzer
-
-__all__ = ["StockAnalyzer"]
